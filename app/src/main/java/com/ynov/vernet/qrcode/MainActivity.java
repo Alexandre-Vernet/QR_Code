aa69a8d8086866a3b7fee53f8bffe1029bf7fc9d
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-27 21:07:12 +0100

    Bon fonctionnement du lecteur QR Code

--- app/src/main/java/com/ynov/vernet/qrcode/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/qrcode/MainActivity.java
@@ -1,14 +1,59 @@
 package com.ynov.vernet.qrcode;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnScanner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // A l'ouverture de l'app
+        scanner();
+
+        // Référencement du bouton
+        btnScanner = findViewById(R.id.btnScanner);
+
+        // Au clic du bouton
+        btnScanner.setOnClickListener(v -> {
+            scanner();
+        });
+    }
+
+
+    public void scanner() {
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+            startActivity(marketIntent);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //handle cancel
+            }
+        }
     }
 }
