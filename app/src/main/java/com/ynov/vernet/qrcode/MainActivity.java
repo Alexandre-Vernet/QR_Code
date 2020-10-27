00bd0e249b66a75a594c112459741d71d7be4235
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-27 20:48:01 +0100

    Création de l'interface

--- app/src/main/java/com/ynov/vernet/qrcode/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/qrcode/MainActivity.java
@@ -1,8 +1,8 @@
 package com.ynov.vernet.qrcode;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
